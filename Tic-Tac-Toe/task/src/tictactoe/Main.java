944e1691a8b3615ace5bb7e775ff3b5bbba83326
Melanie <mmunk1994@gmail.com>
2020-07-21 02:42:57 -0600

    basic output

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,7 +1,38 @@
 package tictactoe;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        Scanner scan = new Scanner(System.in);
+        //possible lines
+        String empty = "     ";
+        String x__ = "X    ";
+        String xx_ = "X X  ";
+        String xxx = "X X X";
+        String o__ = "O    ";
+        String oo_ = "O O  ";
+        String ooo = "O O O";
+        String xo_ = "X O  ";
+        String ox_ = "O X  ";
+        String xox = "X O X";
+        String xoo = "X O O";
+        String oxo = "O X O";
+        String oxx = "O X X";
+        String __x = "    X";
+        String _xx = "  X X";
+        String __o = "    O";
+        String _oo = "  O O";
+        String _xo = "  X O";
+        String _ox = "  O X";
+        String _o_ = "  O  ";
+        String _x_ = "  X  ";
+
+        //output
+        System.out.println(x__);
+        System.out.println(oxx);
+        System.out.println(oo_);
+
+
     }
 }
