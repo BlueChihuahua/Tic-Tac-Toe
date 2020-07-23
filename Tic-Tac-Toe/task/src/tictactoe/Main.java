204c0f4f34d1de27fbec8d647f42f981b7fe5d65
Melanie <mmunk1994@gmail.com>
2020-07-23 00:28:04 -0600

    updating method needs for stage 4

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public enum GameState {
         NotFinished, Draw, XWins, OWins, Impossible
     }
+    //merge field check into enum states
+
     public static void main(String[] args) {
         // write your code here
         Scanner scan = new Scanner(System.in);
